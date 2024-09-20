1aa0f06284b05c964908d28fbd7a41e8d82cfaed
ekinalaydin <ekinaly06@gmail.com>
2024-09-20 21:20:17 +0300

    Add Stack Pop Method

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,13 +1,25 @@
 public static void main(String[] args) {
 
-    Stack myStack = new Stack(2);
+    Stack myStack = new Stack(5);
     System.out.println("Before push():");
     System.out.println("--------------");
     myStack.printAll();
-    myStack.push(1);
+    myStack.push(6);
 
     System.out.println("\n\nAfter push():");
     System.out.println("-------------");
     myStack.printAll();
+    System.out.println("-------------");
+
+    myStack.push(4);
+    System.out.println("Popped: ");
+
+    // (2) Items - Returns 1 Node
+    System.out.println(myStack.pop().value);
+    // (1) Item - Returns 2 Node
+    System.out.println(myStack.pop().value);
+    // (0) Items - Returns null
+    System.out.println(myStack.pop());
+
 
 }
--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -17,4 +17,65 @@ public class Stack {
         top = newNode; // set the new node as the top of the stack
         height = 1;
     }
+    public void push (int value){
+        Node newNode = new Node(value);
+        if(height == 0){ // If the stack is empty, set the new node as the top
+            top = newNode;
+        }else{
+            newNode.next = top; // Otherwise, set the next node of the new node as the current top node,
+            // and then set the new node as the new top node
+            top = newNode;
+        }
+        height++;
+    }
+
+    public Node pop(){
+        if(height == 0){
+            return null;
+        }else{
+            Node temp;
+            temp = top;
+            top = top.next;
+            temp.next = null;
+            height--;
+            return temp;
+
+        }
+    }
+
+    public Node getTop() {
+        return top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (height == 0) {
+            System.out.println("Top: null");
+        } else {
+            System.out.println("Top: " + top.value);
+        }
+        System.out.println("Height:" + height);
+        System.out.println("\nStack:");
+        if (height == 0) {
+            System.out.println("empty");
+        } else {
+            printStack();
+        }
+    }
+
+    public void makeEmpty() {
+        top = null;
+        height = 0;
+    }
 }
